7bf73c01fb8602d1e030017b7b8cecaddcc7b852
Tony Atkins <tony@raisingthefloor.org>
2018-04-18 14:00:09 +0200

    Added static AbleData search.

---
+++ abledata-sandbox/js/md-common.js
@@ -0,0 +1,71 @@
+// A base gradeName that provides a markdown-parsing helper for handlebars.
+//
+// NOTE:  This grade should not be used directly, you should add either the server or client appropriate gradeName to the appropriate list of components.
+//
+//  For your convenience, here are those grade names:
+//  _server_: `gpii.handlebars.helper.md.server`
+//  _client_: `gpii.handlebars.helper.md.client`
+/* eslint-env node */
+"use strict";
+var fluid = fluid || require("infusion");
+var gpii  = fluid.registerNamespace("gpii");
+fluid.registerNamespace("gpii.handlebars.helper.md");
+
+gpii.handlebars.helper.md.getMdFunction = function (that) {
+    return function (context) {
+        if (context) {
+            if (that && that.renderer) {
+                // We need to ensure that this is a string, as Pagedown cannot handle anything else.
+                return that.renderer.render(String(context));
+            }
+            else {
+                fluid.fail("Can't convert markdown content because the renderer could not be found.");
+            }
+        }
+
+        // If we can't evolve the output, we just pass it through.
+        return context;
+    };
+};
+
+// TODO: See if this is still needed.
+// gpii.handlebars.helper.md.configureConverter = function (that) {
+//     if (that.converter) {
+//         // Double all single carriage returns so that they result in new paragraphs, at least for now
+//         that.converter.hooks.chain("preConversion", function (text) {
+//             if (typeof text === "string") {
+//                 return text.replace(/[\r\n]+/g, "\n\n");
+//             }
+//
+//             return text;
+//         });
+//     }
+//     else {
+//         fluid.fail("Could not initialize pagedown converter.  Markdown content will not be parsed.");
+//     }
+// };
+
+fluid.defaults("gpii.handlebars.helper.md", {
+    gradeNames: ["gpii.handlebars.helper"],
+    helperName: "md",
+    markdownItOptions: {},
+    members: {
+        renderer: null
+    },
+    invokers: {
+        "getHelper": {
+            "funcName": "gpii.handlebars.helper.md.getMdFunction",
+            "args":     ["{that}"]
+        }
+    },
+    events: {
+        "rendererAvailable": null
+    }
+    // ,
+    // listeners: {
+    //     "rendererAvailable": {
+    //         funcName: "gpii.handlebars.helper.md.configureConverter",
+    //         args:     ["{that}"]
+    //     }
+    // }
+});
